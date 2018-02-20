c41de14bbdbdbfcee21e5f4495298037e12bd81f
Martin Zlámal <mrtnzlml@gmail.com>
2018-02-20 19:59:05 +0200

    UI: add Touchable component
    This component wraps iOS and Android implementations of touchable components.

---
+++ src/shared/ui/Touchable.js
@@ -0,0 +1,61 @@
+// @flow
+
+import * as React from 'react';
+import {
+  Platform,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import { type StylePropType } from '../index';
+
+type Props = {
+  // not exact: additional props allowed for TouchableOpacity and TouchableNativeFeedback
+  children: React.Node,
+  style?: StylePropType,
+  pressColor?: string,
+};
+
+const ANDROID_VERSION_LOLLIPOP = 21;
+
+export default class Touchable extends React.Component<Props> {
+  static defaultProps = {
+    pressColor: 'rgba(0, 0, 0, .32)',
+  };
+
+  render = () => {
+    // necessary only because of TouchableNativeFeedback
+    const children = React.Children.only(this.props.children);
+
+    /*
+     * TouchableNativeFeedback.Ripple causes a crash on old Android versions,
+     * therefore only enable it on Android Lollipop and above.
+     *
+     * All touchables on Android should have the ripple effect according to
+     * platform design guidelines.
+     * We need to pass the background prop to specify a borderless ripple effect.
+     */
+    if (
+      Platform.OS === 'android' &&
+      Platform.Version >= ANDROID_VERSION_LOLLIPOP
+    ) {
+      const { style, ...rest } = this.props;
+      return (
+        <TouchableNativeFeedback
+          useForeground={TouchableNativeFeedback.canUseNativeForeground()}
+          {...rest}
+          style={null}
+          background={TouchableNativeFeedback.Ripple(
+            this.props.pressColor,
+            false,
+          )}
+        >
+          <View style={style}>{children}</View>
+        </TouchableNativeFeedback>
+      );
+    }
+
+    return <TouchableOpacity {...this.props}>{children}</TouchableOpacity>;
+  };
+}
