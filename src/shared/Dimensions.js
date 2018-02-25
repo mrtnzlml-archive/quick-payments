f392cc68da0650449606aff2cf54afdd7308f335
Martin Zlámal <mrtnzlml@gmail.com>
2018-02-25 19:03:30 +0200

    Add new Dimensions wrapper

---
+++ src/shared/Dimensions.js
@@ -0,0 +1,12 @@
+// @flow
+
+import { Dimensions } from 'react-native'; // eslint-disable-line no-restricted-imports
+
+type DimensionsType = {|
+  width: number,
+  height: number,
+|};
+
+export default {
+  get: (): DimensionsType => Dimensions.get('screen'),
+};
