0bdeb2dff5b206262d8cdf3305d0f5ea87f2aaed
Martin Zlámal <mrtnzlml@gmail.com>
2018-03-01 15:33:03 +0200

    EmailInput: add basic validation rule

--- src/shared/forms/EmailInput.js
+++ src/shared/forms/EmailInput.js
@@ -1,21 +1,61 @@
 // @flow
 
 import * as React from 'react';
+import _ from 'lodash';
+import { Translation } from 'mobile-quick-payments-translations';
 
 import TextInput from './TextInput';
-import { Translation } from '../../translations';
+import StyleSheet from '../ui/PlatformStyleSheet';
+import Colors from '../ui/Colors';
 
 type Props = {|
   placeholder: React.Element<typeof Translation>,
 |};
 
-export default class EmailInput extends React.Component<Props> {
+type State = {|
+  isValid: boolean,
+|};
+
+export default class EmailInput extends React.Component<Props, State> {
+  state = {
+    isValid: true,
+  };
+
+  /**
+   * Please note: this actually doesn't validate the text to be an input.
+   * Why? Because nobody cares. Nobody is going to enter email like
+   * "áéíóúý@ÁÉÍÓÚÝð" into an email field by accident, and that is all
+   * front-end validation is for: to prevent people from accidentally
+   * entering the wrong bit of information, such as their name, in an
+   * email field.
+   *
+   * Email must be validated through verification link.
+   */
+  validateEmail = (emailAddress: string): boolean => {
+    return /^(\S+@\S+|)$/.test(emailAddress); // empty string is considered valid
+  };
+
+  handleInputChange = (text: string) => {
+    this.setState({
+      isValid: this.validateEmail(text),
+    });
+  };
+
   render = () => {
     return (
       <TextInput
         keyboardType="email-address"
         placeholder={this.props.placeholder}
+        onChangeText={_.debounce(this.handleInputChange, 1000)}
+        style={this.state.isValid ? undefined : styleSheet.validationError}
       />
     );
   };
 }
+
+const styleSheet = StyleSheet.create({
+  validationError: {
+    borderColor: Colors.error,
+    borderWidth: 1,
+  },
+});
---
+++ src/shared/forms/__tests__/EmailInput.test.js
@@ -0,0 +1,19 @@
+// @flow
+
+import EmailInput from '../EmailInput.js';
+
+it('validates basic emails', () => {
+  // please note - purpose of this validation is not to verify the email but to avoid mistakes
+  const input = new EmailInput();
+
+  // valid addresses
+  expect(input.validateEmail('')).toBe(true);
+  expect(input.validateEmail('a@b')).toBe(true);
+  expect(input.validateEmail('a@b@c')).toBe(true);
+  expect(input.validateEmail('email@gmail.com')).toBe(true);
+
+  // clearly invalid addresses
+  expect(input.validateEmail('ab')).toBe(false);
+  expect(input.validateEmail(' a@b')).toBe(false); // leading space
+  expect(input.validateEmail('a@b ')).toBe(false); // trailing space
+});
