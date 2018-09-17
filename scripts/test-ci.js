6b05c85356884442b3f4954d8da27f7876dbef45
Martin Zlámal <mrtnzlml@gmail.com>
2018-09-17 18:02:28 +0200

    Add 'scripts/test-ci.js'

---
+++ scripts/test-ci.js
@@ -0,0 +1,20 @@
+// @flow
+
+const {spawnSync} = require('child_process');
+
+const _x = (command, args) => {
+  const {status} = spawnSync(command, args, {stdio: 'inherit'});
+  process.exit(status);
+};
+
+_x('yarn', ['lint']);
+_x('yarn', ['flow']);
+_x('node', [
+  'node_modules/jest/bin/jest.js',
+  '--ci',
+  '--color',
+  '--logHeapUsage',
+  '--maxWorkers=4',
+  '--config=.jest.json',
+]);
+_x('yarn', ['relay', '--validate']);
