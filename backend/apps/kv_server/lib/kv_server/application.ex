8e492551dedb89069122836d0fc00f1fa96ea3b7
Martin Zlámal <mrtnzlml@gmail.com>
2018-09-22 22:18:18 +0200

    BE: finish  example

--- backend/apps/kv_server/lib/kv_server/application.ex
+++ backend/apps/kv_server/lib/kv_server/application.ex
@@ -6,14 +6,13 @@ defmodule KVServer.Application do
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
+    port = String.to_integer(System.get_env("PORT") || "4040")
+
     children = [
-      # Starts a worker by calling: KVServer.Worker.start_link(arg)
-      # {KVServer.Worker, arg},
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      Supervisor.child_spec({Task, fn -> KVServer.accept(port) end}, restart: :permanent)
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
     Supervisor.start_link(children, opts)
   end
