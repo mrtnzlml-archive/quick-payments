71a7f3a2885ac0141a9d52c3fe764ba847220c7f
Martin Zlámal <mrtnzlml@gmail.com>
2018-10-08 15:27:10 +0200

    Fix broken NPM builds
    I didn't transpile __DEV__ constant correctly. Now, there is a test for it so it won't happen again.

--- frontend/scripts/_x.js
+++ frontend/scripts/_x.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // @flow
 
 const {spawnSync} = require('child_process');
@@ -5,6 +6,9 @@ const {spawnSync} = require('child_process');
 const _x = (command /*: string */, args /*: string[] */) => {
   const {status} = spawnSync(command, args, {stdio: 'inherit'});
   if (status !== 0) {
+    console.error(`Command "${command}" exited with status code: ${status}`);
+    console.error(command, args.join(' '));
+
     process.exit(status);
   }
 };
--- frontend/scripts/publish-npm-packages.js
+++ frontend/scripts/publish-npm-packages.js
@@ -1,5 +1,7 @@
 // @flow
 
+const fs = require('fs');
+const glob = require('glob');
 const path = require('path');
 const prompts = require('prompts');
 const rimraf = require('rimraf');
@@ -7,8 +9,8 @@ const rimraf = require('rimraf');
 const _x = require('./_x');
 
 const choices = [
-  // title => package name
-  // value => subdirectory in npm
+  // title: package name
+  // value: subdirectory in npm
   {title: '@mrtnzlml/fetch', value: 'fetch'},
   {title: '@mrtnzlml/relay', value: 'relay'},
   {title: '@mrtnzlml/utils', value: 'utils'},
@@ -30,15 +32,34 @@ prompts({
 function publishPackage(packageName) {
   const buildDirPackage = path.join(buildDir, packageName);
 
+  const additionalBabelPlugins = [
+    // transforms __DEV__ => process.env.NODE_ENV
+    path.join(__dirname, '../packages/babel/dev-expression.js'),
+    // so we don't require global regenerator runtime
+    'babel-plugin-transform-runtime',
+  ];
+
   _x('yarn', [
     'babel',
     `npm/${packageName}`,
     `--out-dir=${buildDirPackage}`,
     '--copy-files',
     '--include-dotfiles', // TODO: does not copy .npmignore (?)
-    `--plugins=${path.join(__dirname, '../packages/babel/dev-expression.js')}`, // transforms __DEV__ => process.env.NODE_ENV
-    '--plugins=babel-plugin-transform-runtime', // so we don't require global regenerator runtime
+    `--plugins=${additionalBabelPlugins.join(',')}`,
   ]);
+
+  const filenames = glob.sync(`${buildDirPackage}/**/*.js`);
+  filenames.forEach(filename => {
+    if (fs.readFileSync(filename).includes('__DEV__')) {
+      // eslint-disable-next-line no-console
+      console.error(
+        'Bundled code contains __DEV__ constant. This is no-go because it indicates faulty Babel' +
+          ' process.',
+      );
+      process.exit(1);
+    }
+  });
+
   _x('cp', ['scripts/templates/.npmignore', `${path.join(buildDirPackage, '.npmignore')}`]);
   _x('yarn', ['test', buildDirPackage]);
 
