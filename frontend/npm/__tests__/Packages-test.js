53e47d62a1164b23f350ab4a339eecdc6cefce05
Martin Zlámal <mrtnzlml@gmail.com>
2018-10-24 14:22:30 +0200

    NPM: add homepage links

---
+++ frontend/npm/__tests__/Packages-test.js
@@ -0,0 +1,27 @@
+// @flow
+
+import path from 'path';
+import glob from 'glob';
+
+test('all NPM package.json files', done => {
+  expect.hasAssertions();
+
+  glob(path.join(__dirname, '..', './**/package.json'), (error, filenames) => {
+    expect(error).toBe(null);
+    expect(filenames).toHaveLength(3);
+
+    filenames.forEach(filename => {
+      // $FlowIssue: https://github.com/facebook/flow/issues/2692
+      const packageJson = require(filename);
+      expect(packageJson.private).toBe(false);
+      expect(packageJson.name).toStartWith('@mrtnzlml/');
+      expect(packageJson.version).toBeString();
+      expect(packageJson.main).toBeString();
+      expect(packageJson.homepage).toStartWith(
+        'https://github.com/mrtnzlml/quick-payments/tree/master/frontend/npm/',
+      );
+    });
+
+    done();
+  });
+});
