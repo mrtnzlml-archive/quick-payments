908b8ea8705fb7b531fd4b29a9a5753a5e498e82
Martin Zlámal <mrtnzlml@gmail.com>
2019-01-15 09:28:56 -0600

    Fetch: use different strategy to polyfill 'Object.setPrototypeOf'
    The previous solution was not good enough because 'instanceof' didn't work after the transpilation.

--- frontend/src/npm/fetch/src/TimeoutError.js
+++ frontend/src/npm/fetch/src/TimeoutError.js
@@ -1,11 +1,13 @@
 // @flow
 
+import setPrototypeOf from './setPrototypeOf';
+
 /**
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error#Custom_Error_Types
  */
 function TimeoutError(message?: string) {
   const instance = new Error(message);
-  Object.setPrototypeOf(instance, Object.getPrototypeOf(this));
+  setPrototypeOf(instance, Object.getPrototypeOf(this));
   if (Error.captureStackTrace) {
     Error.captureStackTrace(instance, TimeoutError);
   }
@@ -21,11 +23,6 @@ TimeoutError.prototype = Object.create(Error.prototype, {
   },
 });
 
-if (Object.setPrototypeOf) {
-  Object.setPrototypeOf(TimeoutError, Error);
-} else {
-  // $FlowExpectedError: mutating this prototype is unsupported (?)
-  TimeoutError.__proto__ = Error; // eslint-disable-line no-proto
-}
+setPrototypeOf(TimeoutError, Error);
 
 module.exports = TimeoutError;
