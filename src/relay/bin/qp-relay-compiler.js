7727df532a4336ba9aabadf363ab5a183ae9644a
Martin Zlámal <mrtnzlml@gmail.com>
2019-03-16 11:31:50 +0100

    Relay: add 'qp-relay-compiler' binary

---
+++ src/relay/bin/qp-relay-compiler.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const spawn = require('child_process').spawn;
+
+// TODO: get Schema and print it first
+
+// TODO: check that `yarn` binary actually exists in this system
+spawn(
+  'yarn',
+  [
+    'relay-compiler',
+    '--src=./src',
+    '--schema=./src/schema.graphql',
+    '--artifactDirectory=./src/__generated__',
+    '--persist-output=./src/persisted-queries.json',
+    '--verbose',
+  ],
+  {
+    stdio: 'inherit',
+  },
+);
